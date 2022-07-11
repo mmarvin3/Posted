b9b597f6444f8d740b97760e599c64bb68a38a4b
Marvin Merida <marvinmerida@Marvins-MBP.fios-router.home>
2022-07-11 15:22:25 -0400

    more routes added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Poster():
+    def __init__(self, name, type, description):
+        self.name = name
+        self.type = type
+        self.description = description
+
+posters = [
+    Poster('Rush Hour', 'movie', 'A poster of Jackie Chan and Chris Tucker starring in the Rush Hour movie'),
+    Poster('Succession', 'tv show', 'A poster of the all the characters in the show Succession'),
+    Poster('Harry Potter', 'book', 'A poster of the book cover of the first Harry Potter book')
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-
+from .models import Poster
 from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+class Poster():
+    def __init__(self, name, type, description):
+        self.name = name
+        self.type = type
+        self.description = description
+
+posters = [
+    Poster('Rush Hour', 'movie', 'A poster of Jackie Chan and Chris Tucker starring in the Rush Hour movie'),
+    Poster('Succession', 'tv show', 'A poster of the all the characters in the show Succession'),
+    Poster('Harry Potter', 'book', 'A poster of the book cover of the first Harry Potter book')
+]
+
+def posters_index(request):
+  return render(request, 'posters/index.html', { 'posters': posters })
